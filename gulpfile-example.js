01aece3dd5efb3576548992082130025c6baf20f
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-10-18 22:25:00 +0300

    add_js_task

--- gulpfile-example.js
+++ gulpfile-example.js
@@ -1,9 +1,26 @@
 'use strict';
 
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 
-gulp.task('default', function() {
+gulp.task('js', function() {
 
-    return gulp.src('client/www/**/*.*')
+    return gulp.src([
+            'client/www/js/**/*.js',
+            '!client/www/js/vendor/*.js'
+        ])
+        .pipe(babel({
+            plugins: ["object-assign"],
+            blacklist: ['strict']
+        }))
+        .pipe(gulp.dest('server/www/js'));
+});
+
+gulp.task('default', ['js'], function() {
+
+    return gulp.src([
+            'client/www/**/*.*',
+            '!client/www/**/*.js'
+        ])
         .pipe(gulp.dest('server/www'));
 });
